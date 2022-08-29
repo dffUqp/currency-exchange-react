e74276f21a15c4621274ce30e8e1da4a80f6df27
dffUqp <dffuqp@gmail.com>
2022-08-29 06:03:04 +0300

    edit folder structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Exchanger from './components/Exchanger/Exchanger';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import './sass/app.scss';
+import '../src/assets/scss/app.scss';
 
 function App() {
   return (
